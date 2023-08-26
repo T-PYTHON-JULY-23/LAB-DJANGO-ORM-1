2a88f26775944fbc014ea2898d328aeaa643c60c
AbdulazizAlzahrani <abdulaziz.alzahrani.14@outlook.com>
2023-08-27 00:23:14 +0300

    handel any none-unique title

--- Blog/main/views.py
+++ Blog/main/views.py
@@ -12,12 +12,19 @@ def blog_view(request:HttpRequest) -> HttpResponse:
 
 def post_view(request:HttpRequest) -> HttpResponse:
     if request.method == "POST":
-        new_post = Post(title = request.POST["title"], 
-                        content = request.POST["content"], 
-                        category = request.POST["category"], 
-                        publish_date = request.POST["publish_date"])
-        new_post.save()
-        return redirect('blog_view')
+        form = CreatePostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            if Post.objects.filter(title=title).exists():
+                form.add_error('title', 'A post with this title already exists.')
+                return render(request, 'main/post.html', {'form' : form})
+            else:
+                new_post = Post(title = request.POST["title"], 
+                                content = request.POST["content"], 
+                                category = request.POST["category"], 
+                                publish_date = request.POST["publish_date"])
+                new_post.save()
+                return redirect('blog_view')
     else:
         form = CreatePostForm()
         return render(request, 'main/post.html', {'form' : form})
