69b967445da1aeecf16e48515b91c494ad666145
renad1 <renadxx17@gmail.com>
2023-08-28 01:39:29 +0300

    lab

--- blog/main/views.py
+++ blog/main/views.py
@@ -18,8 +18,42 @@ def add_post_view(request: HttpRequest):
 
 
 def all_posts_view(request: HttpRequest):
-
     posts = Post.objects.all()
+    
 
     return render(request, "main/posts.html", context = {"posts" : posts})
-    # return render(request, "main/posts.html")
+
+def post_detail_view(request: HttpRequest,post_id):
+
+    post = Post.objects.get(id=post_id)
+
+    return render(request, "main/post_detail.html", context = {"post" : post})
+
+def update_view(request: HttpRequest,post_id):
+
+    post = Post.objects.get(id=post_id)
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        post.category = request.POST["category"]
+        post.publish_date = request.POST["publish_date"]
+        post.save()
+
+        return redirect("main:post_detail_view", post_id= post.id)
+
+    return render(request, "main/update_post.html",{"post" : post})
+
+def delete_view(request: HttpRequest,post_id):
+
+    post = Post.objects.get(id=post_id)
+    post.delete()
+
+    return redirect("main:all_posts_view")
+
+# def filter_view(request:HttpRequest):
+#     if request.method == "POST":
+#         user_input = request.POST['user_input']
+#         post = Post.objects.filter(title__contains=user_input)
+
+
+#     return render("main/filter_post.html",{"post" : post,})
