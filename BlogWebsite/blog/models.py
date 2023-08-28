e50dbb322c605c73cbd4767509f033edcc453295
Lenah11 <lenhh11@gmail.com>
2023-08-28 08:34:24 +0300

    LAB-DJANGO-ORM-2

--- BlogWebsite/blog/models.py
+++ BlogWebsite/blog/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class post(models.Model):
+
+class Post(models.Model):
+    category_choices = (('programing language','programing language'),('DataBase','DataBase'),('artificial intelligence','artificial intelligence'),('internet of things','internet of things'))
+
     title = models.CharField(max_length=2048)
     content = models.TextField()
-    category = models.CharField(max_length=256)
+    category = models.CharField(max_length=256 , choices=category_choices)
     publish_date =models.DateTimeField()
